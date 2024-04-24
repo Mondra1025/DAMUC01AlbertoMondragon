e230aa2f246a652b1383e1c4a19dfcf027cfb141
Alberto Mondragon <ismael20_mondragon@hotmail.com>
2024-04-24 11:03:05 -0500

    Creacion de Correo electronico 2 commit

---
+++ app/src/main/java/com/example/damuc01_alberto_mondragon/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.damuc01_alberto_mondragon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
